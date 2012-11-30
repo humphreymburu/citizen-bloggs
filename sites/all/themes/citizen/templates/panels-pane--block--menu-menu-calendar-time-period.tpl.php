d438c1e430511baa01147b2157d8d3d37aeb06c6
manojkumar1989 <manoj.k@cubeflick.com>
2012-11-30 17:18:08 +0530

    commited the latest files

---
+++ sites/all/themes/citizen/templates/panels-pane--block--menu-menu-calendar-time-period.tpl.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @file panels-pane.tpl.php
+ * Main panel pane template
+ *
+ * Variables available:
+ * - $pane->type: the content type inside this pane
+ * - $pane->subtype: The subtype, if applicable. If a view it will be the
+ *   view name; if a node it will be the nid, etc.
+ * - $title: The title of the content
+ * - $content: The actual content
+ * - $links: Any links associated with the content
+ * - $more: An optional 'more' link (destination only)
+ * - $admin_links: Administrative links associated with the content
+ * - $feeds: Any feed icons or associated with the content
+ * - $display: The complete panels display object containing all kinds of
+ *   data including the contexts and all of the other panes being displayed.
+ */
+?>
+<?php if ($pane_prefix): ?>
+  <?php print $pane_prefix; ?>
+<?php endif; ?>
+<div class="<?php print $classes; ?>" <?php print $id; ?>>
+  <?php if ($admin_links): ?>
+    <?php print $admin_links; ?>
+  <?php endif; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($feeds): ?>
+    <div class="feed">
+      <?php print $feeds; ?>
+    </div>
+  <?php endif; ?>
+
+ 
+  <div class="calendar-action-menu">
+  <select id="action-menu">
+  <option>Actions</option>
+  <?php 
+  
+  //dpr($content);
+	  foreach($content as $menu => $menu_item)
+	  {
+	  	if(isset($menu_item['#title']) && $menu_item['#title'] != "<")
+	  	{	
+	  		echo "<option value='".$menu_item['#original_link']['href']."'>".$menu_item['#title']."</option>";
+	  	}
+	  }	
+	  ?>
+  </select>
+  </div>
+
+  <script type="text/javascript">
+		$("#action-menu").change(function(){
+
+			window.location.href = "/"+$(this).val() 	
+			
+		});
+  </script>
+  
+  <?php if ($links): ?>
+    <div class="links">
+      <?php print $links; ?>
+       </div>
+  
+  <?php endif; ?>
+
+  <?php if ($more): ?>
+    <div class="more-link">
+      <?php print $more; ?>
+    </div>
+  <?php endif; ?>
+</div>
+<?php if ($pane_suffix): ?>
+  <?php print $pane_suffix; ?>
+<?php endif; ?>
