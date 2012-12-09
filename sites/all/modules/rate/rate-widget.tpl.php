d2af7d1c1cbb5aa9eec5bebab35b0710c4f70867
Tom Flick <cubeflick@gmail.com>
2012-12-09 23:24:54 +0530

    Some css changes and vote count improvements

--- sites/all/modules/rate/rate-widget.tpl.php
+++ sites/all/modules/rate/rate-widget.tpl.php
@@ -39,11 +39,17 @@
  */
 ?>
 
-<?php print theme('item_list', array('items' => $buttons)); ?>
 
-<?php if ($info): ?>
-  <?php print '<div class="rate-info">' . $info . '</div>'; ?>
-<?php endif; ?>
+
+<div class="upvote">
+	<?php print theme('item_list', array('items' => $buttons)); ?>
+</div>
+  
+<?php print '<div class="rate-info">' . $results['count'] . '</div>'; ?>
+
+<div class="downvote">
+	<?php print theme('item_list', array('items' => $buttons)); ?>
+</div>
 
 <?php if ($display_options['description']): ?>
   <?php print '<div class="rate-description">' . $display_options['description'] . '</div>'; ?>
